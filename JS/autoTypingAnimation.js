e63aff7a9e3a9bb033af6e2b49e93d1fdceacc60
Maya Jagadale <mayajagadale@gmail.com>
2026-08-18 15:20:24 +0530

    Add portfolio

---
+++ JS/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span> Hi, I am Jagadale Maya </span>  I’m a passionate MERN Stack Developer with a strong interest in building modern, responsive, and user-friendly web applications. I have completed my BCA and developed full-stack projects using MongoDB, Express.js, React.js, and Node.js. <br/>thanks 😊. ",  
+         ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
